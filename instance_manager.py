ef60ef5899dfeebfaa85069775bbe06d4b131006
Sebastian Varga <sebastian@tech-stack.co>
2023-08-14 16:41:19 +0200

    Use either local aws config or config ini file

--- instance_manager.py
+++ instance_manager.py
@@ -2,50 +2,51 @@ import boto3
 import sys
 import configparser
 
-# Load AWS configurations from the .ini file
 def load_config():
     config = configparser.ConfigParser()
     config.read('config.ini')
 
-    # Check if the necessary section and keys exist in the config file
-    if 'AWS' not in config:
-        raise Exception('Config file missing or "AWS" section not found in config.ini')
-
-    return {
-        'aws_access_key': config['AWS']['aws_access_key'],
-        'aws_secret_key': config['AWS']['aws_secret_key'],
-        'region': config['AWS']['region']
+    aws_config = {
+        'aws_access_key': None,  # Default to None
+        'aws_secret_key': None,
+        'region': None
     }
 
-# List all the EC2 instances along with their details
+    # Only load values from config if they exist, otherwise keep None
+    if 'AWS' in config:
+        aws_config['aws_access_key'] = config['AWS'].get('aws_access_key')
+        aws_config['aws_secret_key'] = config['AWS'].get('aws_secret_key')
+        aws_config['region'] = config['AWS'].get('region')
+
+    return aws_config
+
 def list_instances(aws_config):
-    ec2 = boto3.resource('ec2', region_name=aws_config['region'],
-                         aws_access_key_id=aws_config['aws_access_key'],
-                         aws_secret_access_key=aws_config['aws_secret_key'])
+    ec2 = boto3.resource('ec2', 
+                         region_name=aws_config['region'],
+                         aws_access_key_id=aws_config.get('aws_access_key'),
+                         aws_secret_access_key=aws_config.get('aws_secret_key'))
     for instance in ec2.instances.all():
         print(f"ID: {instance.id}, State: {instance.state['Name']}, Type: {instance.instance_type}")
 
-# Start the specified EC2 instance
 def start_instance(instance_id, aws_config):
-    ec2 = boto3.client('ec2', region_name=aws_config['region'],
-                       aws_access_key_id=aws_config['aws_access_key'],
-                       aws_secret_access_key=aws_config['aws_secret_key'])
+    ec2 = boto3.client('ec2', 
+                       region_name=aws_config['region'],
+                       aws_access_key_id=aws_config.get('aws_access_key'),
+                       aws_secret_access_key=aws_config.get('aws_secret_key'))
     response = ec2.start_instances(InstanceIds=[instance_id])
     print(f"Starting instance: {instance_id}")
 
-# Stop the specified EC2 instance
 def stop_instance(instance_id, aws_config):
-    ec2 = boto3.client('ec2', region_name=aws_config['region'],
-                       aws_access_key_id=aws_config['aws_access_key'],
-                       aws_secret_access_key=aws_config['aws_secret_key'])
+    ec2 = boto3.client('ec2', 
+                       region_name=aws_config['region'],
+                       aws_access_key_id=aws_config.get('aws_access_key'),
+                       aws_secret_access_key=aws_config.get('aws_secret_key'))
     response = ec2.stop_instances(InstanceIds=[instance_id])
     print(f"Stopping instance: {instance_id}")
 
 if __name__ == "__main__":
-    # Load AWS configurations from the .ini file
     aws_config = load_config()
 
-    # Check for the presence of command line arguments
     if len(sys.argv) < 2:
         print("Usage: script.py <command> [<instance_id>]")
         print("Commands: list, start, stop")
@@ -53,7 +54,6 @@ if __name__ == "__main__":
 
     command = sys.argv[1].lower()
 
-    # Handle different commands: list, start, stop
     if command == "list":
         list_instances(aws_config)
     elif command == "start":
